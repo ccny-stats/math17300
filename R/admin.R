84f8d2025129ec907f75d353e033ca77b163c939
Vitaly Druker <vitaly@westendstats.com>
2023-08-30 11:30:50 -0400

    finished lecture 2 and homework

---
+++ R/admin.R
@@ -0,0 +1,4 @@
+get_lecture_homework <- function(lecture_number) {
+    schedule_raw <- readxl::read_xlsx(here::here('resources','semester-planning.xlsx'))
+    subset(schedule_raw, `Lecture Number` == lecture_number, select = exercises, drop = TRUE)
+}
