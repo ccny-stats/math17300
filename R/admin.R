c2c51b693558ff29cebeeae7e054cd98da5d3925
Vitaly Druker <vitaly@westendstats.com>
2023-08-31 09:52:48 -0400

    push lecture 2 and some other small updates

--- R/admin.R
+++ R/admin.R
@@ -2,3 +2,5 @@ get_lecture_homework <- function(lecture_number) {
     schedule_raw <- readxl::read_xlsx(here::here('resources','semester-planning.xlsx'))
     subset(schedule_raw, `Lecture Number` == lecture_number, select = exercises, drop = TRUE)
 }
+
+ggplot2::theme_set(ggplot2::theme_linedraw())
